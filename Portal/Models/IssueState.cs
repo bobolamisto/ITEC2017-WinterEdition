39089e296b940df997cf4f08d3c85221994cf043
Vorobet Bogdan <bogdanvorobet@gmail.com>
2017-12-08 20:06:38 +0200

    Add Comment, IssueState, User_Issue

---
+++ Portal/Models/IssueState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Models
+{
+    public enum StateType
+    {
+       Active,
+       Archived,
+       Solved
+    }
+    public class IssueState
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public StateType Type { get; set; }
+        public IssueState()
+        {
+
+        }
+    }
+}
---
+++ Portal/Models/User_Issue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Models
+{
+    public enum VoteType
+    {
+        Upvote,
+        Downvote,
+        None
+    }
+    public class User_Issue
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int IssueId { get; set; }
+        public bool IsAuthor { get; set; }
+        public VoteType Vote { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual User User { get; set; }
+        public virtual Issue Issue { get; set; }
+
+        public User_Issue()
+        {
+            Comments = new List<Comment>();
+        }
+
+    }
+}
