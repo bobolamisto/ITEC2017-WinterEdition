b177370daae044340e83a48b32f684e697024959
Alina <alina.toaderr@yahoo.com>
2017-12-10 00:30:53 +0200

    draw aria of interest on the issues map

--- Portal/wwwroot/js/allIissues.js
+++ Portal/wwwroot/js/allIissues.js
@@ -14,9 +14,24 @@ function showPosition(position) {
 function initMap() {
     var loc = { lat: window.centerlat, lng: window.centerlong };
     window.map = new google.maps.Map(document.getElementById('all-issues-map'), {
-        zoom: 7,
+        zoom: 9,
         center: loc
     });
+
+    var me = new google.maps.Marker({
+        position: loc,
+        map: window.map,
+        //title: 'your location',
+        animation: google.maps.Animation.BOUNCE,
+    });
+
+    var circle = new google.maps.Circle({
+        map: map,
+        radius: window.myradius * 1000,    // 10 miles in metres
+        fillColor: '#AA0000'
+    });
+    circle.bindTo('center', me, 'position');
+
     for (var i= 0; i < window.issues.length; i++){
         var marker = new google.maps.Marker({
             position: { lat: window.issues[i].location.latitude, lng: window.issues[i].location.longitude },
@@ -24,6 +39,7 @@ function initMap() {
             //title: 'your location',
             //animation: google.maps.animation.drop,
         });
+        
     }
 
 }
